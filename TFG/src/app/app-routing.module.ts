6e71c78fafccccb0ca27cecc07b7afb5b964a8e9
David Infante Casas <davidinfante1997@gmail.com>
2020-03-21 04:07:15 +0100

    Revert "Header menu added both PC and mobile"
    This reverts commit dd8b9cc958ed6d169c451bd16e9f2a9a76a6ad7e.

--- TFG/src/app/app-routing.module.ts
+++ TFG/src/app/app-routing.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-  {
-    path: 'inicio',
-    loadChildren: () => import('./components/pages/inicio/inicio.module').then(m => m.InicioPageModule)
-  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
 ];
 
 @NgModule({
--- TFG/src/app/home/home.page.spec.ts
+++ TFG/src/app/home/home.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { InicioPage } from './inicio.page';
+import { HomePage } from './home.page';
 
-describe('InicioPage', () => {
-  let component: InicioPage;
-  let fixture: ComponentFixture<InicioPage>;
+describe('HomePage', () => {
+  let component: HomePage;
+  let fixture: ComponentFixture<HomePage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InicioPage ],
+      declarations: [ HomePage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(InicioPage);
+    fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
